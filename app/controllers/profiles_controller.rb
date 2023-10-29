dc3f9c0bbd3553c31d0464ac0ccc3ea577775744
山崎柊 <>
2023-10-29 14:17:55 +0900

    ダミーユーザーの作成。ダミーユーザーのプロフィールを作成。

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,28 +1,25 @@
 class ProfilesController < ApplicationController
 	before_action :authenticate_user!
-	before_action :set_profile
 
 	def show
+		@user = User.find(params[:id])
 	end
 
 	def edit
 	end
 
 	def update
-		if @profile.update(profile_params)
-			image_url = rails_blob_url(@profile.avatar, disposition: "attachment")
+		profile = current_user.profile
+		if profile.update(profile_params)
+			image_url = rails_blob_url(profile.avatar, disposition: "attachment")
 			render json: { status: 'success', image_url: image_url }
 		else
-			render json: { status: 'error', errors: @profile.errors.full_messages }, status: :unprocessable_entity
+			render json: { status: 'error', errors: profile.errors.full_messages }, status: :unprocessable_entity
 		end
 	end
 
-  private
-  def profile_params
-    params.require(:profile).permit(:username, :avatar)
-  end
-
-	def set_profile
-    @profile = current_user.profile
-  end
+	private
+	def profile_params
+		params.require(:profile).permit(:username, :avatar)
+	end
 end
