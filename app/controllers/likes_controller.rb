d5dcddd5a41d44eab7a7f237e36efacd7d3eb196
山崎柊 <>
2023-10-31 21:00:18 +0900

    like状態がリロード時に表示されるように修正。

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,7 +1,10 @@
 class LikesController < ApplicationController
-    before_action :set_article
-  
-    def create
+  before_action :set_article
+
+  def create
+    if current_user.has_liked?(@article)
+      render json: { error: 'Already liked' }, status: :unprocessable_entity
+    else
       @like = @article.likes.build(user: current_user)
       if @like.save
         render json: { status: 'liked' }
@@ -9,21 +12,20 @@ class LikesController < ApplicationController
         render json: { error: 'Failed to like' }, status: :unprocessable_entity
       end
     end
-  
-    def destroy
-      @like = @article.likes.find_by(user_id: current_user.id)
-      if @like&.destroy
-        # ... 何らかの成功時の処理 ...
-        render json: { status: 'unliked' }
-      else
-        render json: { error: 'Failed to unlike' }, status: :unprocessable_entity
-      end
-    end
-  
-    private
-  
-    def set_article
-      @article = Article.find(params[:article_id])
+  end
+
+  def destroy
+    @like = @article.likes.find_by(user_id: current_user.id)
+    if @like&.destroy
+      render json: { status: 'unliked' }
+    else
+      render json: { error: 'Failed to unlike' }, status: :unprocessable_entity
     end
   end
-  
+
+  private
+
+  def set_article
+    @article = Article.find(params[:article_id])
+  end
+end
--- app/javascript/packs/like.js
+++ app/javascript/packs/like.js
@@ -1,38 +1,36 @@
 import axios from 'axios';
-
 document.addEventListener('DOMContentLoaded', () => {
-    const likeIcons = document.querySelectorAll('.like-icons');
-  
-    likeIcons.forEach((iconContainer) => {
-      const articleId = iconContainer.dataset.articleId;  // HTMLにdata-article-idを設定しておく必要があります
-      const inactiveIcon = iconContainer.querySelector('.like-button-inactive');
-      const activeIcon = iconContainer.querySelector('.like-button-active');
-  
-      inactiveIcon.addEventListener('click', () => {
-        axios.post(`/articles/${articleId}/like`)
-          .then((response) => {
-            if (response.data.status === 'liked') {
-              inactiveIcon.style.display = 'none';
-              activeIcon.style.display = 'block';
-            }
-          })
-          .catch((error) => {
-            console.error('Error liking the article:', error);
-          });
-      });
-  
-      activeIcon.addEventListener('click', () => {
-        axios.delete(`/articles/${articleId}/like`)
-          .then((response) => {
-            if (response.data.status === 'unliked') {
-              inactiveIcon.style.display = 'block';
-              activeIcon.style.display = 'none';
-            }
-          })
-          .catch((error) => {
-            console.error('Error unliking the article:', error);
-          });
-      });
+  const likeIcons = document.querySelectorAll('.like-icons');
+
+  likeIcons.forEach((iconContainer) => {
+    const articleId = iconContainer.dataset.articleId;
+    const inactiveIcon = iconContainer.querySelector('.like-button-inactive');
+    const activeIcon = iconContainer.querySelector('.like-button-active');
+
+    inactiveIcon.addEventListener('click', () => {
+      axios.post(`/articles/${articleId}/like`)
+        .then((response) => {
+          if (response.data.status === 'liked') {
+            inactiveIcon.style.display = 'none';
+            activeIcon.style.display = 'block';
+          }
+        })
+        .catch((error) => {
+          console.error('Error liking the article:', error);
+        });
+    });
+
+    activeIcon.addEventListener('click', () => {
+      axios.delete(`/articles/${articleId}/like`)
+        .then((response) => {
+          if (response.data.status === 'unliked') {
+            inactiveIcon.style.display = 'block';
+            activeIcon.style.display = 'none';
+          }
+        })
+        .catch((error) => {
+          console.error('Error unliking the article:', error);
+        });
     });
   });
-  
+});
