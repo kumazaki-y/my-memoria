f7672aea18cf1f5053a88526a44148c7216cfd45
山崎柊 <>
2023-11-02 08:20:10 +0900

    いいね機能のリクエストスペック。

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -1,22 +1,21 @@
 include ActionDispatch::TestProcess
 
 FactoryBot.define do
-    factory :article do
+  factory :article do
+    association :user
 
     transient do
-        images_count { 0 }
+      images_count { 0 }
     end
 
-        after(:build) do |article, evaluator|
-            evaluator.images_count.times do
-                article.images.attach(
-                io: StringIO.new('dummy image content'),
-                filename: 'dummy_image.jpg',
-                content_type: 'image/jpg'
-                )
-            end
-        end
+    after(:build) do |article, evaluator|
+      evaluator.images_count.times do
+        article.images.attach(
+          io: StringIO.new('dummy image content'),
+          filename: 'dummy_image.jpg',
+          content_type: 'image/jpg'
+        )
+      end
     end
+  end
 end
-
-  
