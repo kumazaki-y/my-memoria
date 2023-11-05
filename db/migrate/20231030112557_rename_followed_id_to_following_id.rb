62ed2ce07bd53e8a87ce2bd760f7821a96032339
山崎柊 <>
2023-11-05 14:59:53 +0900

    タイプミスを修正

--- db/migrate/20231030112557_rename_followed_id_to_following_id.rb
+++ db/migrate/20231030112557_rename_followed_id_to_following_id.rb
@@ -1,5 +1,5 @@
 class RenameFollowedIdToFollowingId < ActiveRecord::Migration[6.0]
   def change
-    rename_column :relationships, :followied_id, :following_id
+    rename_column :relationships, :followed_id, :following_id
   end
 end
