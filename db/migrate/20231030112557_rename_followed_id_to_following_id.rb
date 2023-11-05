6c4760047706f45575fef32daea6d81ad44172c0
山崎柊 <>
2023-11-05 14:49:28 +0900

    クラス名の誤記を修正。

--- db/migrate/20231030112557_rename_followed_id_to_following_id.rb
+++ db/migrate/20231030112557_rename_followed_id_to_following_id.rb
@@ -1,5 +1,5 @@
-class RenameFollowingIdToFollowingId < ActiveRecord::Migration[6.0]
+class RenameFollowedIdToFollowingId < ActiveRecord::Migration[6.0]
   def change
-    rename_column :relationships, :following_id, :following_id
+    rename_column :relationships, :followied_id, :following_id
   end
 end
