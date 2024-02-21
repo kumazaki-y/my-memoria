36b5b5d3ceea2e3bd53ae81234e54bc774b61681
山崎柊 <>
2024-02-22 07:16:43 +0900

    ゲストログインのリクエストをPOSTに変更

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   }
 
   devise_scope :user do
-    get 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
