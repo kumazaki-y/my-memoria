ddb69786f305341cf52e5efcd80f298515664eb0
山崎柊 <>
2024-02-24 09:44:35 +0900

    アプリ名を変更。それに伴うコード修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "kumazaki_insta_app_production"
+  # config.active_job.queue_name_prefix = "my_memoria_production"
 
   config.action_mailer.perform_caching = false
 
