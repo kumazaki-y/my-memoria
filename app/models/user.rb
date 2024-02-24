45324d9917c0e83a2d07a1e703a51bcea4e88a5c
山崎柊 <>
2024-02-24 14:14:57 +0900

    ゲストユーザーのログアウト時にユーザー・投稿・いいね・コメントを削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,9 +9,10 @@ class User < ApplicationRecord
   validates :password, presence: true, length: { in: 6..20 }
 
   has_one :profile, dependent: :destroy
-  has_many :articles
-  has_many :likes
-  has_many :comments
+  has_many :articles, dependent: :destroy
+  has_many :likes, dependent: :destroy
+  has_many :comments, dependent: :destroy
+
 
   has_many :following_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   has_many :following, through: :following_relationships, source: :following
